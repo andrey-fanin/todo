9c9bfcf555f097c3de9e23408d5ec0a1163fefc6
Andrey Fanin <andr.fanin16@yandex.ru>
2022-06-22 22:15:40 +0500

    rewrited template, now it's pug WHY??? idk

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,79 @@
-<template>
-    <header class="header">
-        <div class="container">
-            <div class="logo">ToDo</div>
-            <div class="gh-logo">
-                <a href="https://github.com/andrey-fanin/todo" class="gh-link">
-                    <div class="gh-link__img">
-                        <img src="./assets/gh-logo.png" alt="github octocat logo">
-                    </div>
-                </a>
-            </div>
-            <div class="form">
-                <input type="text" :placeholder="placeholder" v-model.trim="handleInput" @keypress.enter="addTask">
-                <button class="btn" @click="addTask">Add new task</button>
-            </div>
-        </div>
-    </header>
-    <div class="container container--main">
-        <div class="container container--mob">
-            <transition name="rotateY">
-                <div class="form form--mob" v-if="isActive">
-                    <textarea type="text" :placeholder="placeholder" v-model.trim="handleInput" @keypress.enter="addTask"></textarea>
-                    <button class="btn" @click="addTask">+</button>
-                </div>
-            </transition>
-            <div class="buttons__wrap">
-                <button class="btn btn--wide" @click="checkActive">{{ isActive ? 'close' : 'add new task' }}</button>
-                <transition name="smoothFade">
-                    <button class="btn btn--transparent" v-if="isActive" @click="clearHandleInput">clear text</button>
-                </transition>
-            </div>
-        </div>
-        <div class="buttons__wrap buttons__wrap--remove">
-            <div>
-                <transition name="fadeX">
-                    <div v-if="todoList.length">
-                        <button class="btn btn-remove" @click="clearTodo">clear todo</button>
-                    </div>
-                </transition>
-            </div>
-            <div>
-                <transition name="fadeX">
-                    <div v-if="doneList.length">
-                        <button class="btn btn-remove" @click="clearDone">clear done</button>
-                    </div>
-                </transition>
-            </div>
-        </div>
-        <div class="tasks-title__wrap">
-            <h2>To do</h2>
-            <span class="tasks-title__wrap-num" :class="{ active: getDoge }">
-                <transition name="numberScale">
-                    <div :key="todoList.length">
-                      {{ todoList.length }}
-                    </div>
-                </transition>
-            </span>
-        </div>
-        <ul class="task-list">
-            <li v-for="(task, idx) in todoList" :key="task.id">
-                <label>
-                    <input type="checkbox" @change="check(idx, 'active')">
-                    <div>{{ task.title }}</div>
-                </label>
-                <button class="btn btn-remove" @click="removeItem(idx, 'active')">remove</button>
-            </li>
-        </ul>
-        <transition name="jumpIn">
-            <div class="doge--wrapper" v-if="isDoge" @click="checkDoge">
-                <div class="doge--wrapper__img">
-                    <img src="./assets/doge.png" alt="super dog" title="puper dog">
-                </div>
-            </div>
-        </transition>
-        <div class="tasks-title__wrap">
-            <h2>Done</h2>
-            <span class="tasks-title__wrap-num">
-                <transition name="numberScale">
-                    <div :key="doneList.length">
-                      {{ doneList.length }}
-                    </div>
-                </transition>
-            </span>
-        </div>
-        <ul class="task-list task-list--done">
-            <li v-for="(task, idx) in doneList" :key="task.id">
-                <label>
-                    <input type="checkbox" checked @change="check(idx, 'completed')">
-                    <div>{{ task.title}}</div>
-                </label>
-                <button class="btn btn-remove" @click="removeItem(idx, 'completed')">remove</button>
-            </li>
-        </ul>
-        <div class="advices__wrap" v-if="showAdvice">
-            <TransitionGroup name="smoothFade">
-                <div class="advices__wrap-emoji" v-html="emoji" :key="emoji"></div>
-                <p class="advices__wrap-text" :key="advice">
-                    {{ advice }}
-                </p>
-            </TransitionGroup>
-        </div>
-    </div>
-    <footer class="footer">
-        <div class="container container--footer">
-        </div>
-    </footer>
+<template lang="pug">
+header.header
+    .container
+        .logo ToDo
+        .gh-logo
+            a.gh-link(href='https://github.com/andrey-fanin/todo')
+                .gh-link__img
+                    img(src='./assets/gh-logo.png'
+                    alt='github octocat logo')
+        .form
+            input(type='text'
+                :placeholder='placeholder'
+            v-model.trim='handleInput'
+                @keypress.enter='addTask')
+            button.btn(@click='addTask') Add new task
+.container.container--main
+    .container.container--mob
+        transition(name='rotateY')
+            .form.form--mob(v-if='isActive')
+                textarea(type='text'
+                    :placeholder='placeholder'
+                v-model.trim='handleInput'
+                    @keypress.enter='addTask')
+                button.btn(@click='addTask') +
+        .buttons__wrap
+            button.btn.btn--wide(@click='checkActive') {{ isActive ? 'close' : 'add new task' }}
+            transition(name='smoothFade')
+                button.btn.btn--transparent(v-if='isActive'
+                    @click='clearHandleInput') clear text
+    .buttons__wrap.buttons__wrap--remove
+        div
+            transition(name='fadeX')
+                div(v-if='todoList.length')
+                    button.btn.btn-remove(@click='clearTodo') clear todo
+        div
+            transition(name='fadeX')
+                div(v-if='doneList.length')
+                    button.btn.btn-remove(@click='clearDone') clear done
+    .tasks-title__wrap
+        h2 To do
+        span.tasks-title__wrap-num(:class='{ active: getDoge }')
+            transition(name='numberScale')
+                div(:key='todoList.length') {{ todoList.length }}
+    ul.task-list
+        li(v-for='(task, idx) in todoList' :key='task.id')
+            label
+                input(type='checkbox'
+                    @change="check(idx, 'active')")
+                div {{ task.title }}
+            button.btn.btn-remove(@click="removeItem(idx, 'active')") remove
+    transition(name='jumpIn')
+        .doge--wrapper(v-if='isDoge' @click='checkDoge')
+            .doge--wrapper__img
+                img(src='./assets/doge.png'
+                alt='super dog'
+                title='puper dog')
+    .tasks-title__wrap
+        h2 Done
+        span.tasks-title__wrap-num
+            transition(name='numberScale')
+                div(:key='doneList.length') {{ doneList.length }}
+    ul.task-list.task-list--done
+        li(v-for='(task, idx) in doneList' :key='task.id')
+            label
+                input(type='checkbox'
+                checked
+                @change="check(idx, 'completed')")
+                div {{ task.title}}
+            button.btn.btn-remove(@click="removeItem(idx, 'completed')") remove
+    .advices__wrap(v-if='showAdvice')
+        transition-group(name='smoothFade')
+            .advices__wrap-emoji(v-html='emoji'
+                :key='emoji')
+            p.advices__wrap-text(:key='advice') {{ advice }}
+footer.footer
+    .container.container--footer
 </template>
 
 <script>
